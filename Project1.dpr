ea646f095b117b97d65c881ef0945763e81ba5b3
gustavoroads <farzimrodrigues123@gmail.com>
2026-02-15 11:22:19 -0300

    Primeiro Commit

---
+++ Project1.dpr
@@ -0,0 +1,14 @@
+program Project1;
+
+uses
+  Vcl.Forms,
+  uPrincipal in 'uPrincipal.pas' {fPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfPrincipal, fPrincipal);
+  Application.Run;
+end.
